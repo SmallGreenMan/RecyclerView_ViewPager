fce4074caa02c2beb833a3f49af0e3ea26fbf654
Alexandr Voishel <avoishel@gmail.com>
2022-01-19 18:46:57 +0300

    add: addOnPageChangeListener and logs

--- app/src/main/java/com/gmail/avoishel/recyclerview_viewpager_test/MainActivity.kt
+++ app/src/main/java/com/gmail/avoishel/recyclerview_viewpager_test/MainActivity.kt
@@ -2,8 +2,10 @@ package com.gmail.avoishel.recyclerview_viewpager_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Switch
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -32,6 +34,36 @@ class MainActivity : AppCompatActivity() {
 
         val viewPager : ViewPager =  findViewById<ViewPager>(R.id.viewPager)
         viewPager.adapter = ViewPagerAdapter(supportFragmentManager)
+
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+
+            // - дает информацию о текущем занчении позиции скрола или пролистывания
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                Log.d("viewPager onPageScrolled", "$position $positionOffset $positionOffsetPixels")
+            }
+
+            // - onPageSelected - Дает номер текущей отображенной страници
+            override fun onPageSelected(position: Int) {
+                Log.d("viewPager onPageSelected", position.toString())
+                Toast.makeText(this@MainActivity,
+                    "$position ${viewPager.currentItem}",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            // - Сообщает нам о сотоянии в котором находится скроллер
+            //  0 --- scrollStateEagle - ничего не скролится
+            //  1 --- scrollStateDragen - это пользователь ращит страницу
+            //  2 --- scrollStateSeting - это скроллер долисывает страницу до конца
+            override fun onPageScrollStateChanged(state: Int) {
+                Log.d("viewPager onPageScrollStateChanged", state.toString())
+            }
+
+        })
     }
 
     inner class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm){
